dbd46f54514b2e5ecbf6ff75c428271c64a9cc29
Killua <thomas.nguyen3@mail.mcgill.ca>
2022-12-23 19:11:12 -0500

    Basic functions created

---
+++ index.js
@@ -0,0 +1,20 @@
+function add(a, b) {
+    return a + b;
+};
+
+function substract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    return a / b;
+};
+
+function operate(operation, a, b) {
+    return operation(a, b);
+};
+
