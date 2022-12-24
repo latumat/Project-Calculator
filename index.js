f4dbd5789a5f3c8e244668175b613b75e8748004
latumat <killua31048@gmail.com>
2022-12-24 00:22:05 -0500

    Functional calculator

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+var stored = document.querySelectorAll('.num');
+var operands = document.querySelectorAll('.opp');
+var screen = document.querySelector('.top');
+var clearButton = document.querySelector('.ac');
+var lastNum = '';
+var nextNum = '';
+var position = 1;
+var op = '';
+var eqStatus = false;
+
 function add(a, b) {
     return a + b;
 };
@@ -18,3 +28,67 @@ function operate(operation, a, b) {
     return operation(a, b);
 };
 
+function storedNumber() {
+    stored.forEach((s) => {
+        s.addEventListener('click', () => {
+            if (position === 1) {
+                lastNum = lastNum + s.textContent;
+            } else {
+                nextNum = nextNum + s.textContent;
+            }
+            screen.innerHTML = lastNum + op + nextNum;
+            console.log(lastNum)
+        });
+    });
+};
+
+function operandClick() {
+    operands.forEach((operand) => {
+        operand.addEventListener('click', () => {
+            if (operand.textContent === '=') {
+                eqStatus = true;
+                result()
+            } else {
+                position = 2
+                op = operand.textContent;
+                screen.innerHTML = lastNum + op + nextNum;
+                console.log(op);
+            }
+        });
+    })
+}
+
+function result() {
+    var func = ''
+    if (op === '÷') {
+        func = divide;
+    } else if (op === 'x') {
+        func = multiply;
+    } else if (op === '-') {
+        func = substract;
+    } else {
+        func = add;
+    };
+    lastNum = Math.round(operate(func, parseFloat(lastNum), parseFloat(nextNum))*1000000000) / 1000000000;
+    nextNum = ''
+    screen.innerHTML = lastNum;
+    eqStatus = false;
+}
+
+function clear() {
+    clearButton.addEventListener('click', () => {
+        lastNum = ''
+        nextNum = ''
+        op = ''
+        position = 1
+        screen.innerHTML = ''
+    })
+
+}
+function calculator() {
+    storedNumber()
+    operandClick()
+    clear()
+}
+
+calculator()
